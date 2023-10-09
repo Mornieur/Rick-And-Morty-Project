3970a857ae4c4096a93a2283cf294ca23fed8c0a
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-09 15:31:53 -0300

    feat: create a modal of characters information

---
+++ src/components/ModalInformation/index.tsx
@@ -0,0 +1,41 @@
+import * as S from './styles';
+import { Modal } from '../Modal';
+import { Carousel } from '../Carousel';
+import { NameBox } from '@/app/consult/styles';
+import { ReactNode } from 'react';
+
+interface ModalInformationProps {
+  info: string;
+  nameBox: string;
+
+  children: ReactNode;
+  isModalOpen: boolean;
+  onCloseModal: () => void;
+}
+
+export const ModalInformation: React.FC<ModalInformationProps> = ({
+  info = '',
+  nameBox = '',
+  isModalOpen,
+  onCloseModal,
+  children,
+}) => {
+  return (
+    <>
+      <S.ModalInformation isModalOpen={isModalOpen}>
+        <Modal
+          setCloseModal={onCloseModal}
+          closeButton
+          backgroundColor="#233329"
+          maxWidth="700px"
+        >
+          <S.EpisodeContainer>
+            <NameBox>{nameBox}</NameBox>
+            <S.Info>{info}</S.Info>
+            <Carousel>{children}</Carousel>
+          </S.EpisodeContainer>
+        </Modal>
+      </S.ModalInformation>
+    </>
+  );
+};
