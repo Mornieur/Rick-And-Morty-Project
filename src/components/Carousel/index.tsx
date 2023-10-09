c4a6f3f72f98dc6768b45306d3b3cdcce6c0573e
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-09 15:26:29 -0300

    feat: create a global carousel component

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,46 @@
+import * as S from './styles';
+import { ReactNode, Children, useRef } from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+
+interface CarouselProps {
+  children: ReactNode;
+}
+
+const SLIDE_SCROLL = 184;
+
+export const Carousel: React.FC<CarouselProps> = ({ children }) => {
+  const childrenArray = Children.toArray(children);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const nextSlide = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft += SLIDE_SCROLL;
+    }
+  };
+
+  const prevSlide = () => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollLeft -= SLIDE_SCROLL;
+    }
+  };
+
+  return (
+    <S.Container>
+      <S.Button onClick={prevSlide}>
+        <IoIosArrowBack size={40} />
+      </S.Button>
+
+      <S.Content>
+        <S.DataItems ref={scrollRef}>
+          {childrenArray.map((child, index) => (
+            <S.Slide key={index}>{child}</S.Slide>
+          ))}
+        </S.DataItems>
+      </S.Content>
+
+      <S.Button onClick={nextSlide}>
+        <IoIosArrowForward size={40} />
+      </S.Button>
+    </S.Container>
+  );
+};
