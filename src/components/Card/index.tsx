c904cf8ff37c60300a0152736d871bc4138b27e2
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-09 15:15:23 -0300

    feat: create a global card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import * as S from './styles';
+import { FC } from 'react';
+import { CharacterTypes } from '@/services/GET/types';
+import { IColors } from './styles';
+import { BiPlanet } from 'react-icons/bi';
+
+interface CardProps extends CharacterTypes {}
+
+export const Card: FC<CardProps> = ({
+  image,
+  name,
+  status,
+  origin,
+  id,
+}: CardProps) => {
+  return (
+    <S.View>
+      <S.Container key={id}>
+        <S.DataCharacter>
+          <S.CharacterImage
+            src={image}
+            alt="person image"
+            width={150}
+            height={150}
+          />
+          <S.Name>{name}</S.Name>
+        </S.DataCharacter>
+
+        <S.DataPerson>
+          <S.Dimension>
+            <BiPlanet size={20} color="#5D595E" />
+            {origin.name}
+          </S.Dimension>
+          <S.Status typeColor={status as IColors}>{status}</S.Status>
+        </S.DataPerson>
+      </S.Container>
+    </S.View>
+  );
+};
