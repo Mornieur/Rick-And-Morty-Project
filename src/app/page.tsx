b61b7110595a4f21cfed5c52330611995b0344b8
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-09 15:11:24 -0300

    feat: create a initial page with characters anime cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,72 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
+import { NextPage } from 'next';
+import useCharacterData from '@/services/GET/useCharactersQuery';
+import { Card } from '@/components/Card';
+import * as S from '../styles/characters/styles';
+import { Search } from '@/components/SearchComponent';
+import { useState } from 'react';
+import RickAndMortyLogo from '../../public/Rick-And-Morty-Logo.png';
+import { Loading } from '@/global/components/Loading';
+import InfiniteScroll from '@/components/InfiniteScroll';
+
+const Characters: NextPage = () => {
+  const { data, isLoading, isError, hasNextPage, fetchNextPage } =
+    useCharacterData();
+
+  const [characterContent, setCharacterContent] = useState({
+    value: '',
+  });
+
+  const onHandleSearch = (value: string) => {
+    setCharacterContent({
+      ...characterContent,
+      value: value,
+    });
+  };
+
+  const filteredCharacters = data?.pages
+    .flat()
+    .filter((character) =>
+      character.name
+        .toLowerCase()
+        .includes(characterContent.value.toLowerCase())
+    );
+
+  const showLoadingError =
+    characterContent.value.length > 0 && filteredCharacters?.length === 0;
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <S.Container>
+      <S.RickAndMortyLogo
+        src={RickAndMortyLogo.src}
+        alt="rick and morty logo"
+        width={300}
+        height={300}
+      />
+      <Search
+        value={characterContent.value}
+        onChange={(value) => onHandleSearch(value)}
+      />
+      <S.CardsContainer>
+        {data?.pages
+          .flat()
+          .filter((character) =>
+            character.name
+              .toLowerCase()
+              .includes(characterContent.value.toLowerCase())
+          )
+          .map((character) => (
+            <S.CardLink key={character.id} href={`/consult?id=${character.id}`}>
+              <Card {...character} />
+            </S.CardLink>
+          ))}
+        {isLoading && <Loading />}
+        {showLoadingError && <Loading label="No results found" />}
+        <InfiniteScroll loadMore={() => fetchNextPage()} />
+      </S.CardsContainer>
+    </S.Container>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Characters;
