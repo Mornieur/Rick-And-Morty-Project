91772287cd532e2e59e5966e6feb62537d7de1da
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-09 15:45:43 -0300

    style: styling characters page

---
+++ src/styles/characters/styles.ts
@@ -0,0 +1,29 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: auto;
+  background-color: #233329;
+  flex-direction: column;
+  display: flex;
+  align-items: center;
+`;
+
+export const CardLink = styled.a`
+  max-width: 430px;
+  width: 100%;
+`;
+
+export const CardsContainer = styled.div`
+  padding: 1rem;
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RickAndMortyLogo = styled.img`
+  width: 100%;
+  object-fit: contain;
+`;
