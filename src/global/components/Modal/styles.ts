1b284a0d8c9f78559ca2d51e915e3d44898d87eb
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-25 10:32:16 -0300

    style: add modal styles in global folder

---
+++ src/global/components/Modal/styles.ts
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+interface ModalStyles {
+  maxWidth: string;
+  backgroundColor: string;
+}
+
+export const Container = styled.div<ModalStyles>`
+  width: 100%;
+  max-width: ${({ maxWidth }) => maxWidth || '500px'};
+  height: 100%;
+  padding: 2rem;
+  margin: 1rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 1rem;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  z-index: 999;
+  position: relative;
+
+  @media (max-width: 1200px) {
+    padding: 1rem;
+    margin: 0.5rem;
+    max-width: 90%;
+    height: auto;
+  }
+
+  @media (max-width: 480px) {
+    max-width: 100%;
+    border-radius: 0;
+  }
+`;
+
+export const CloseButton = styled.button`
+  cursor: pointer;
+  position: absolute;
+  right: 1rem;
+  top: 1rem;
+  color: #fff;
+  background-color: transparent;
+  border: none;
+
+  @media (max-width: 768px) {
+    right: 0.5rem;
+    top: 0.5rem;
+    font-size: 1.5rem;
+  }
+`;
