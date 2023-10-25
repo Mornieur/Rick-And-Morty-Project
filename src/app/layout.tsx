b812c00e62b065979475be2ad2b24a6757f1dd64
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-25 10:25:38 -0300

    refactor: changing metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Rick and Morty',
-  description: 'Rick and morty api',
+  description: 'Rick and Morty api',
 };
 
 export default function RootLayout({
