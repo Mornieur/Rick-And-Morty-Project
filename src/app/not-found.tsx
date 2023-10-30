4d00a4b9e85ae936b44d9913c0f23bd6d560a595
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-30 00:37:12 -0300

    feat: add rick easter egg into galaxy

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,11 +3,12 @@
 import { useRouter } from 'next/navigation';
 import * as S from '../styles/error/styles';
 import Error404 from '../../public/error404.png';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Galaxy } from '@/models/Galaxy';
 import { StarsShip } from '@/models/Starship';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { Metadata } from 'next';
+import { RickModel } from '@/models/RickModel';
 
 export const metadata: Metadata = {
   title: 'Página 404',
@@ -15,43 +16,47 @@ export const metadata: Metadata = {
 };
 
 export default function NotFound() {
-  const router = useRouter();
+  const [showGalaxyOnly, setShowGalaxyOnly] = useState(false);
 
   useEffect(() => {
     const galaxy = new Galaxy();
-
     const scene = galaxy.scene;
     const camera = galaxy.camera;
     const renderer = galaxy.renderer;
 
     const starshipModel = new StarsShip(scene);
-
     starshipModel.load();
 
-    new OrbitControls(camera, renderer.domElement);
+    const rickModel = new RickModel(scene);
 
-    const animate = () => {
-      requestAnimationFrame(animate);
-      renderer.render(scene, camera);
-      starshipModel.starsMesh.rotation.x += 0.0005;
-    };
+    rickModel.load(() => {
+      new OrbitControls(camera, renderer.domElement);
 
-    animate();
+      const animate = () => {
+        requestAnimationFrame(animate);
+
+        rickModel.update();
+
+        renderer.render(scene, camera);
+        starshipModel.starsMesh.rotation.x += 0.0005;
+      };
+
+      animate();
+    });
   }, []);
+
   return (
     <S.Container>
-      <S.ErrorImage src={Error404.src} alt="imagem de erro" />
-      <p>Página não encontrada...</p>
+      {!showGalaxyOnly && (
+        <>
+          <S.ErrorImage src={Error404.src} alt="imagem de erro" />
+          <p>Page not found...</p>
 
-      <S.HomeButton
-        type="button"
-        onClick={() => {
-          router.push('/');
-          router.refresh();
-        }}
-      >
-        Explore the Galaxy
-      </S.HomeButton>
+          <S.HomeButton type="button" onClick={() => setShowGalaxyOnly(true)}>
+            Explore the Galaxy
+          </S.HomeButton>
+        </>
+      )}
     </S.Container>
   );
 }
