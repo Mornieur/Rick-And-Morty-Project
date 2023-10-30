282349c84e49b626847066cdea48d3fad3952781
Maria Fernanda <mariafernanda-coro@hotmail.com>
2023-10-30 00:37:34 -0300

    feat: create a rick model 3d

---
+++ src/models/RickModel.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+export class RickModel {
+  scene: THREE.Scene;
+  rickMesh?: THREE.Group;
+  loader: GLTFLoader;
+
+  constructor(scene: THREE.Scene) {
+    this.scene = scene;
+    this.loader = new GLTFLoader();
+  }
+
+  update() {
+    if (this.rickMesh) {
+      this.rickMesh.rotation.y += 0.005;
+    }
+  }
+
+  load(callback: () => void) {
+    this.loader.load('/rick.glb', (gltf) => {
+      this.rickMesh = gltf.scene;
+
+      if (this.rickMesh) {
+        this.rickMesh.scale.set(5, 5, 5);
+
+        this.rickMesh.position.set(100, -100, 10);
+
+        this.rickMesh.rotation.y = Math.PI;
+
+        this.rickMesh.rotation.x = Math.PI / 5;
+
+        this.scene.add(this.rickMesh);
+
+        callback();
+      }
+    });
+  }
+}
